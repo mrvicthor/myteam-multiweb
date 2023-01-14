ebde337b9200373dcd75c6c6ead6602803c1c222
mrvicthor <victoreleanya89@gmail.com>
2023-01-14 00:23:32 +0000

    updated testimonials page

--- app/components/testimonials/index.tsx
+++ app/components/testimonials/index.tsx
@@ -14,7 +14,7 @@ const Testimonials = () => {
         "“The team perfectly fit the specialized skill set required. They focused on the most essential features helping us launch the platform eight months faster than planned.”",
       name: "Kady Baker",
       role: "Product Manager at Bookmark",
-      icon: kadyImg,
+      icon: "../../../public/images/avatar-kady.jpg",
     },
     {
       id: 2,
@@ -22,7 +22,7 @@ const Testimonials = () => {
         "“We needed to automate our entire onboarding process. The team came in and built out the whole journey. Since going live, user retention has gone through the roof!”",
       name: "Aiysha Reese",
       role: "Founder of Manage",
-      icon: aiyshaImg,
+      icon: "../../../public/images/avatar-aiysha.jpg",
     },
     {
       id: 3,
@@ -30,7 +30,7 @@ const Testimonials = () => {
         "“Amazing. Our team helped us build an app that delivered a new experience for hiring a physio. The launch was an instant success with 100k downloads in the first month.”",
       name: "Arthur Clarke",
       role: "Co-founder of MyPhysio",
-      icon: arthurImg,
+      icon: "../../../public/images/avatar-arthur.jpg",
     },
   ];
   return (

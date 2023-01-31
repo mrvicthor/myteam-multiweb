1aa9ded79d2b353429edf956e91ff6c4fec1c4d3
mrvicthor <victoreleanya89@gmail.com>
2023-01-31 22:11:24 +0000

    updated tablet design of the about page

--- app/components/director/index.tsx
+++ app/components/director/index.tsx
@@ -1,32 +1,81 @@
+"use client";
+
 import Image from "next/image";
 import crossIcon from "../../../public/images/icon-cross.svg";
+import closeIcon from "../../../public/images/icon-close.svg";
+import { useState } from "react";
+import { FaTwitter, FaLinkedin } from "react-icons/fa";
 
 export interface IProps {
   title: string;
   name: string;
   icon: string;
+  content: string;
 }
 
-const Director = ({ title, name, icon }: IProps) => {
+const Director = ({ title, name, icon, content }: IProps) => {
+  const [showMore, setShowMore] = useState<boolean>(false);
   return (
-    <div className="bg-[#012F34] pt-8 flex flex-col items-center">
-      <div className="relative w-[6rem] h-[6rem] rounded-full">
-        <Image
-          src={icon}
-          alt={name}
-          fill
-          className="rounded-full"
-          sizes="(max-width: 768px) 100vw,
+    <div
+      className={`bg-[#012F34] ${
+        showMore ? "pt-[2.1875rem]" : "pt-8"
+      } flex flex-col items-center`}
+    >
+      {showMore ? (
+        <article className="px-[2.8856rem]">
+          <h3 className="font-bold text-center text-[#79c8c7] leading-[1.75rem] text-[1.125rem]">
+            {name}
+          </h3>
+          <p className="pt-2 text-[0.9375rem] leading-[1.5625rem] font-semibold text-center">
+            {content}
+          </p>
+          <div className="pt-6 flex items-center justify-center space-x-4">
+            <a
+              href="#"
+              className="h-[1.25rem] w-[1.25rem] hover:text-[#f67e7e]"
+            >
+              <FaTwitter />
+            </a>
+            <a
+              href="#"
+              className="h-[1.25rem] w-[1.25rem] hover:text-[#f67e7e]"
+            >
+              <FaLinkedin />
+            </a>
+          </div>
+        </article>
+      ) : (
+        <>
+          <div className="relative w-[6rem] h-[6rem] rounded-full">
+            <Image
+              src={icon}
+              alt={name}
+              fill
+              className="rounded-full"
+              sizes="(max-width: 768px) 100vw,
         (max-width: 1200px) 50vw,
         33vw"
-        />
-      </div>
-      <header className="pt-4 flex flex-col items-center">
-        <h3 className="text-[#79C8C7] font-bold text-lg text-center">{name}</h3>
-        <p className="italic font-medium text-[0.9375rem]">{title}</p>
-      </header>
-      <button className="relative bg-[#f67e7e] h-[3.5rem] w-[3.5rem] rounded-full flex items-center justify-center top-[1.75rem]">
-        <Image src={crossIcon} alt="cross icon" className="rounded-full" />
+            />
+          </div>
+          <header className="pt-4 flex flex-col items-center">
+            <h3 className="text-[#79C8C7] font-bold text-lg text-center">
+              {name}
+            </h3>
+            <p className="italic font-medium text-[0.9375rem]">{title}</p>
+          </header>
+        </>
+      )}
+      <button
+        onClick={() => setShowMore(!showMore)}
+        className={`relative ${
+          showMore ? "bg-[#79C8C7]" : "bg-[#f67e7e]"
+        } h-[3.5rem] w-[3.5rem] rounded-full flex items-center justify-center top-[1.75rem]`}
+      >
+        {showMore ? (
+          <Image src={closeIcon} alt="close icon" className="rounded-full" />
+        ) : (
+          <Image src={crossIcon} alt="cross icon" className="rounded-full" />
+        )}
       </button>
     </div>
   );
--- app/components/directors/index.tsx
+++ app/components/directors/index.tsx
@@ -11,60 +11,77 @@ const Directors = () => {
       name: "Nikita Marks",
       title: "Founder & CEO",
       image: "/images/avatar-nikita.jpg",
+      content:
+        "“It always amazes me how much talent there is in every corner of the globe.”",
     },
     {
       id: 2,
       name: "Cristian Duncan",
       title: "Co-founder & COO",
       image: "/images/avatar-christian.jpg",
+      content:
+        "“Distributed teams required unique processes. You need to approach work in a new way.”",
     },
     {
       id: 3,
       name: "Cruz Hamer",
       title: "Co-founder & CTO",
       image: "/images/avatar-cruz.jpg",
+      content:
+        "“Technology is at the forefront of enabling distributed teams. That's where we come in.”",
     },
     {
       id: 4,
       name: "Drake Heaton",
       title: "Business Development Lead",
       image: "/images/avatar-drake.jpg",
+      content:
+        "“Hiring similar people from similar backgrounds is a surefire way to stunt innovation.”",
     },
     {
       id: 5,
       name: "Griffin Wise",
       title: "Lead Marketing",
       image: "/images/avatar-griffin.jpg",
+      content:
+        "“Unique perspectives shape unique products, which is what you need to survive these days.”",
     },
     {
       id: 6,
       name: "Aden Allan",
-      title: "Founder & CEO",
+      title: "Head of Talent",
       image: "/images/avatar-aden.jpg",
+      content:
+        "“Empowered teams create truly amazing products. Set the north star and let them follow it.” ",
     },
   ];
   return (
     <section className="bg-[#004047] text-[#ffffff] overflow-hidden">
-      <div className="relative -top-[6.25rem] -left-[6.25rem]">
+      <div className="relative -top-[6.25rem] -left-[6.25rem] md:top-0">
         <Image src={bgPattern} alt="background pattern" />
       </div>
-      <div className="container relative z-[10]">
+      <div className="container relative z-[10] md:-top-[6rem]">
         <h2 className="font-bold text-[2rem] leading-[2rem] text-center">
           Meet the directors
         </h2>
-        <div className="grid mt-12 space-y-[3.25rem] pb-[1.75rem]">
+        <div className="grid mt-12 gap-[3.25rem] pb-[1.75rem] md:grid-cols-2 md:gap-x-[0.6875rem] md:px-[4.625rem] md:gap-y-[3rem]">
           {directors.map((director) => (
             <Director
               key={director.id}
               name={director.name}
               icon={director.image}
               title={director.title}
+              content={director.content}
             />
           ))}
         </div>
       </div>
-      <div className="absolute z-[1] top-[150rem] -right-[0.5rem]">
-        <Image src={bgPattern3} alt="background pattern" />
+      <div className="relative z-[1] -top-[6rem] ">
+        <Image
+          src={bgPattern3}
+          alt="background pattern"
+          className="absolute right-[0]"
+        />
       </div>
     </section>
   );
